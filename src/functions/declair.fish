24b9e2758f92220b93446deaac40e2557de707e7
lens0021 <lorentz0021@gmail.com>
2024-05-05 23:40:04 +0900

    Split the main function

---
+++ src/functions/declair.fish
@@ -0,0 +1,19 @@
+function declair
+  argparse --stop-nonopt -- $argv
+
+  if functions --query _declair_$argv[1]
+      _declair_$argv[1] $argv[2..]
+  else
+      _declair_help
+  end
+end
+
+function _declair_help
+  echo "\
+Usage: declair subcommand
+
+Subcommands:
+  update
+  pm
+"
+end
--- src/functions/declair_update.fish
+++ src/functions/declair_update.fish
@@ -1,23 +1,3 @@
-function declair
-    argparse --stop-nonopt -- $argv
-
-    if functions --query _declair_$argv[1]
-        _declair_$argv[1] $argv[2..]
-    else
-        _declair_help
-    end
-end
-
-function _declair_help
-    echo "\
-Usage: declair subcommand
-
-Subcommands:
-    update
-    pm
-"
-end
-
 function _declair_update_rc
     set -f last_updated $_declair_last_updated
     if test -n $last_updated
