864c3da673428e48099822601af77c0157c41cd9
lens0021 <lorentz0021@gmail.com>
2024-10-10 20:44:46 +0900

    Add a skip option

--- src/functions/declair_update.fish
+++ src/functions/declair_update.fish
@@ -3,10 +3,12 @@ function _declair_update_rc
     if test -n $last_updated
         and test (math $last_updated + '60*60*12') -lt (date +%s)
 
-        read -lP 'The last updated date is too old. Check the updates? ' do_update
+        read -lP 'The last updated date is too old. Check the updates? (s to skip)' do_update
         switch $do_update
             case Y y yes
                 _declair_update
+                set -U _declair_last_updated (date +%s)
+            case S s
                 set -U _declair_last_updated (date +%s)
         end
     end
